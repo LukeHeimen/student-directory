4665027070203b68a1e2ac67683aadb081b2ed82
LukeHeimen <kaelmitsuhiko11@gmail.com>
2020-06-16 21:41:04 +0800

    Added function to add student button.

--- gui.py
+++ gui.py
@@ -2,57 +2,80 @@ import PySimpleGUI as sg
 
 sg.theme('Reds')   # Add a touch of color
 
-window = sg.Window('Student Directory', size=(1300, 500),
-                   use_default_focus=False)
-
 button_names = ["Add New Student", "View Student Details",
                 "Edit Student Details", "Delete Student",
                 "Clear Student Directory"]
-button_column = [[sg.Button(button_names[i])] for i in range(5)]
+button_keys = ["-ADD-", "-VIEW-", "-EDIT-", "-DELETE-", "-CLEAR-"]
+button_column = [[sg.Button(button_names[i],
+                  key=button_keys[i])] for i in range(5)]
 
 text_names = ["Student Number", "Name", "Course & Year", "Age",
               "Email Address", "Contact Number"]
 text_column = [[sg.Text(text_names[i])] for i in range(6)]
-text_column += [[sg.Button("Enter")]]
+text_column += [[sg.Button("Enter", disabled=True, key="-ENTER-")]]
 
-input_column = [[sg.InputText(size=(30, 1))] for i in range(6)]
+input_keys = ["-SN-", "-NAME-", "-CY-", "-AGE-", "-EMAIL-", "-CN-"]
+input_column = [[sg.InputText(size=(30, 1), disabled=True,
+                 key=input_keys[i], enable_events=True)] for i in range(6)]
 
-search_column = [[sg.Button("Search")]]
+search_column = [[sg.Button("Search", disabled=True, key="-SEARCH-")]]
 
 left_column = [ [sg.Column(button_column)],
                 [sg.Column(text_column), sg.Column(input_column),
                  sg.Column(search_column)] ]
 
-# data_text = [["1", "201512658", "Luke Heimen", "BSCS 4", "21", "lukeheimen@gmail.com", "+639456160641"],
-#              ["2", "201504445", "Kael Mitsuhiko", "BSCS 4", "21", "kaelmitsuhiko11@gmail.com", "+639456160641"]]
-# data = [[data_text[row][col] for col in range(7)] for row in range(2)]
-data = [["    ", "", "                  ", "", "    ", "                         ", ""]]
+old_data = [ ["    ", "", "                  ", "",
+         "    ", "                         ", ""] ]
 headings = ["No."] + text_names
 
 right_column = [ [sg.Text("Student Directory", size=(100, 1),
                   justification="center")],
-                 [sg.Table(values=data, headings=headings, num_rows=20,
+                 [sg.Table(values=old_data, headings=headings, num_rows=20,
                   justification="left", auto_size_columns=True,
-                  key="-TABLE-")] ]
+                  key="-TABLE-",vertical_scroll_only=False)] ]
 
 layout = [ [sg.Column(left_column, size=(430, 500)),
             sg.VerticalSeparator(),
             sg.Column(right_column, size=(870, 500))] ]
 
 # Create the Window
-window.layout(layout)
+window = sg.Window('Student Directory', layout, size=(1300, 500),
+                   use_default_focus=False)
 window.finalize()
 
 # Event Loop to process "events" and get the "values" of the inputs
+data = []
+data_index = -1
+
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-      break
-    # if event == "Search":
-    #   data_text.append(data_text[0])
-    #   window["-TABLE-"].update(values=data_text)
-    # if i == 0:
 
-    # print(values[0])
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == "-ADD-":
+        for key in input_keys:
+            window[key].update(disabled=False)
+            window["-ENTER-"].update(disabled=False)
+
+    if event == "-ENTER-":
+        if data_index == -1:
+            data_index += 1
+        data += [ [str(data_index+1)] ]
+        for key in input_keys:
+            if values[key]:
+                data[data_index] += [values[key]]
+            else:
+                sg.popup("Fill out all forms first!")
+                data.pop(data_index)
+                data_index -= 1
+                break
+
+        if data_index > -1:
+            window["-TABLE-"].update(values=data)
+        for key in input_keys:
+            window[key].update(value="", disabled=True)
+            window["-ENTER-"].update(disabled=True)
+        data_index += 1
 
 window.close()
