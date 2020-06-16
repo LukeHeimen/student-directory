595df2b58d95cf9e955f1d2da464e0d9e5404c29
LukeHeimen <kaelmitsuhiko11@gmail.com>
2020-06-16 23:53:53 +0800

    Added function to view student details button.

--- gui.py
+++ gui.py
@@ -24,13 +24,13 @@ left_column = [ [sg.Column(button_column)],
                 [sg.Column(text_column), sg.Column(input_column),
                  sg.Column(search_column)] ]
 
-old_data = [ ["    ", "", "                  ", "",
+data = [ ["    ", "", "                  ", "",
          "    ", "                         ", ""] ]
 headings = ["No."] + text_names
 
 right_column = [ [sg.Text("Student Directory", size=(100, 1),
                   justification="center")],
-                 [sg.Table(values=old_data, headings=headings, num_rows=20,
+                 [sg.Table(values=data, headings=headings, num_rows=20,
                   justification="left", auto_size_columns=True,
                   key="-TABLE-",vertical_scroll_only=False)] ]
 
@@ -44,7 +44,7 @@ window = sg.Window('Student Directory', layout, size=(1300, 500),
 window.finalize()
 
 # Event Loop to process "events" and get the "values" of the inputs
-data = []
+data.pop()
 data_index = -1
 
 while True:
@@ -55,8 +55,19 @@ while True:
 
     if event == "-ADD-":
         for key in input_keys:
-            window[key].update(disabled=False)
+            window[key].update(value="", disabled=False)
             window["-ENTER-"].update(disabled=False)
+            window["-SEARCH-"].update(disabled=True)
+    if event == "-VIEW-":
+        for key in input_keys:
+            window[key].update(value="", disabled=True)
+            window["-ENTER-"].update(disabled=True)
+
+        if data:
+            window["-SN-"].update(disabled=False)
+            window["-SEARCH-"].update(disabled=False)
+        else:
+            sg.popup("Student directory is empty!")
 
     if event == "-ENTER-":
         if data_index == -1:
@@ -77,5 +88,23 @@ while True:
             window[key].update(value="", disabled=True)
             window["-ENTER-"].update(disabled=True)
         data_index += 1
+    if event == "-SEARCH-":
+        value = values["-SN-"]
+        counter = 0
+        if value:
+            for data_row in data:
+                if value == data_row[1]:
+                    for i in range(len(input_keys)):
+                        window[input_keys[i]].update(value=data_row[i+1],
+                                                     disabled=True)
+                    window["-SEARCH-"].update(disabled=True)
+                    break
+                else:
+                    counter += 1
+        else:
+            sg.popup("Student number cannot be empty!")
+
+        if counter > 0:
+            sg.popup("Student number does not exist!")
 
 window.close()
